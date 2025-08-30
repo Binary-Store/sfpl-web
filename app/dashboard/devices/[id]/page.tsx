2b9c849c20d34041cf1b2fadcccc866803efd815
harshil-vasoya <harshilvasoya2812@gmail.com>
2025-08-30 15:33:22 +0530

    12 hours time in device by id

--- app/dashboard/devices/[id]/page.tsx
+++ app/dashboard/devices/[id]/page.tsx
@@ -104,7 +104,7 @@ export default function AlarmPanel() {
           <div className="flex items-center gap-2">
             <Clock className="h-4 w-4 text-gray-600" />
             <span className="text-sm font-medium">
-              {alarmData ? new Date(alarmData.timestamp).toLocaleString() : "No data"}
+              {alarmData ? new Date(alarmData.timestamp).toLocaleString("en-US", {day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit"  , second: "2-digit" , hour12: true}) : "No data"}
             </span>
           </div>
         </div>
